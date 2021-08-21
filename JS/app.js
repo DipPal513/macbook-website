ca3ba49e7de3cb68ff7547d99e692c8c34e78e52
DipPal513 <65772938+DipPal513@users.noreply.github.com>
2021-08-21 16:12:13 +0600

    validation

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,11 @@
-// storing all product price
-
+// storing all global variable
 const memoryCost = document.getElementById('extra-memory-cost')
 const storageCost = document.getElementById('extra-storage-cost')
 const deliveryCost = document.getElementById('delivery-charge')
 
-
+const promoInput = document.getElementById('promo-input')
+const promoBtn = document.getElementById('promo-btn');
+const finalTotal = document.getElementById('final-total');
 //calculate product price
 function calculateProductCost(btnId,productTotal,cost){
     const btn = document.getElementById(btnId);
@@ -13,8 +14,7 @@ function calculateProductCost(btnId,productTotal,cost){
         previousProductPrice.innerText = cost
         const previousProductNumber = previousProductPrice.innerText
         calculateTotalCost(cost,previousProductNumber);
-    })
-    
+    })  
 }
 //calling function with valid parameter
 calculateProductCost('16GB-memory-btn','extra-memory-cost',180);
@@ -24,13 +24,10 @@ calculateProductCost('1TB-ssd-btn','extra-storage-cost',180);
 calculateProductCost('256GB-ssd-btn','extra-storage-cost',0);
 calculateProductCost('free-delivery-btn','delivery-charge',0);
 calculateProductCost('charge-delivery-btn','delivery-charge',20);
-
-
 //for calculating total price
 function calculateTotalCost(){
     const total = document.getElementById('total-price');
     const mainPrice = document.getElementById('best-price');
-    const finalTotal = document.getElementById('final-total')
     const extraMemoryCost  = Number(memoryCost.innerText);
     const extraStorageCost = Number(storageCost.innerText)
     const extraDeliveryCost = Number(deliveryCost.innerText) 
@@ -38,18 +35,16 @@ function calculateTotalCost(){
     const grandTotal = mainPriceTotal+extraMemoryCost + extraStorageCost + extraDeliveryCost;
     total.innerText = grandTotal;
     finalTotal.innerText = grandTotal;
-
+    const totalCost = Number(total.innerText)
+    return totalCost;
 }
 //for cupon code
-    const promoInput = document.getElementById('promo-input')
-    const promoBtn = document.getElementById('promo-btn');
-    const finalTotal = document.getElementById('final-total')
-    const finalTotalNumber = parseFloat(finalTotal.innerText);
-        promoBtn.addEventListener('click',function(){
-            if(promoInput.value = 'stevekaku'){
-                const promoCalculation = finalTotalNumber - (finalTotalNumber *20) / 100 
-                finalTotal.innerText = promoCalculation;
-                promoInput.value = '';
-            }
-        })
-    
+promoBtn.addEventListener('click',function(){
+        if(promoInput.value = 'stevekaku'){
+        const totalPrice = calculateTotalCost();
+        console.log(totalPrice)
+        const promoCalculation = totalPrice - (totalPrice *20) / 100 
+        finalTotal.innerText = promoCalculation;
+        promoInput.value = '';
+     }
+})   
